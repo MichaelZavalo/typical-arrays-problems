bb278d189e49671c609536096500e4030bf75f28
MichaelZavalo <mihailzavalo@yandex.ru>
2020-10-02 12:44:39 +0300

    fix: fix test if no params specified or an empty array passed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 exports.min = function min(array) {
     let result = 0;
-    if (array === [] || array.length === 0) {
-        result = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
     } else {
-        result = Math.min(...array);
+        return result = Math.min(...array);
     }
-    return result;
 }
 
 exports.max = function max(array) {
     let result = 0;
-    if (array === [] || array.length === 0) {
-        result = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
     } else {
-        result = Math.max(...array);
+        return result = Math.max(...array);
     }
-    return result;
 }
 
 exports.avg = function avg(array) {
     let result = 0;
-    if (array === [] || array.length === 0) {
-        result = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
     } else {
         let reducer = (accum, currVal) => accum + currVal;
-        result = array.reduce((reducer)) / array.length;
+        return result = array.reduce((reducer),0) / array.length;
     }
-    return result;
 }
