bbea34681bb0d8a01f8ece7336f48da7aa64aed8
MichaelZavalo <mihailzavalo@yandex.ru>
2020-10-02 11:23:27 +0300

    fix: fix method .avg

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-
-exports.min = function min (array) {
-  let result = 0;
-  if (array === [] || array.length === 0){
-    result = 0;
-  } else {
-    result = Math.min(...array);
-  }
-  return result; 
+exports.min = function min(array) {
+    let result = 0;
+    if (array === [] || array.length === 0) {
+        result = 0;
+    } else {
+        result = Math.min(...array);
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  let result = 0;
-  if (array === [] || array.length === 0){
-    result = 0;
-  } else {
-    result = Math.max(... array);
-  }
-return result;
+exports.max = function max(array) {
+    let result = 0;
+    if (array === [] || array.length === 0) {
+        result = 0;
+    } else {
+        result = Math.max(...array);
+    }
+    return result;
 }
 
-exports.avg = function avg (array) {
-  
-  let result = 0;
-  if (array === [] || array.length === 0){
-    result = 0;
-  } else {
-    result = Math.avg(...array);
-  }
-  return result;
+exports.avg = function avg(array) {
+    let result = 0;
+    if (array === [] || array.length === 0) {
+        result = 0;
+    } else {
+        let reducer = (accum, currVal) => accum + currVal;
+        result = array.reduce((reducer)) / array.length;
+    }
+    return result;
 }
