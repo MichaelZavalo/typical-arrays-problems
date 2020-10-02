b370c79e379871188680637297b59842fb2b60af
MichaelZavalo <mihailzavalo@yandex.ru>
2020-10-02 10:44:45 +0300

    feat: add resolve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = Math.min(...array);
+  /* let result = array.forEach(el,i,arr => {
+    if(arr[i]< arr[i + 1]){
+        return arr [i];
+    }
+  });*/
+  return result; 
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = Math.max(... array);
+  /* let result = array.forEach(...array,i,arr => {
+    
+}); */
+return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = Math.avg(...array);
+  /* let result = array.forEach(...array,i,arr => {
+    
+  }); */
+  return result;
 }
